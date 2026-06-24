5465e0d1ce3895752ea64bef18a0be132bd964f2
Thomas Silva <ths2.thomas.com>
2026-06-24 12:35:53 -0300

    feat(auth): protect logs endpoints with JWT guard

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     UsersModule,
+    PassportModule,
     JwtModule.register({
       secret: 'super-secret-key',
       signOptions: {
@@ -16,6 +19,6 @@ import { UsersModule } from '../users/users.module';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
