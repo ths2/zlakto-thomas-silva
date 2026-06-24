b00930c2070266edbfcfa81a53309390110632e6
Thomas Silva <ths2.thomas.com>
2026-06-24 11:21:54 -0300

    chore: setup logs and cloud storage modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { LogsModule } from './logs/logs.module';
+import { CloudModule } from './cloud/cloud.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, LogsModule, CloudModule],
   controllers: [AppController],
   providers: [AppService],
 })
