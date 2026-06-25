7bd9cd4936392d34f8ff35d427963c2d73051c42
Thomas Silva <ths2.thomas.com>
2026-06-25 17:31:26 -0300

    chore: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,11 @@
 // trigger pipeline
-// trigger pipeline 2
+// trigger pipeline 3
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
