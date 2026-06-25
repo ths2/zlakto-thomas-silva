9bd03f5e707cb21f4df4c6243ac567fc27a22572
Thomas Silva <ths2.thomas.com>
2026-06-24 23:28:02 -0300

    feat(infra): add AWS CDK infrastructure for ECR and App Runner

---
+++ infra/aws/lib/aws-stack.ts
@@ -0,0 +1,60 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as apprunner from '@aws-cdk/aws-apprunner-alpha';
+
+export class AwsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // 1. Manter o bucket S3 existente (ou criar um novo se não existir)
+    const logBucket = new s3.Bucket(this, 'CloudLogBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT for production
+      autoDeleteObjects: true, // NOT for production
+    });
+
+    // 2. Criar ECR Repositories
+    const backendRepository = new ecr.Repository(this, 'BackendRepository', {
+      repositoryName: 'cloud-log-access-backend',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const frontendRepository = new ecr.Repository(this, 'FrontendRepository', {
+      repositoryName: 'cloud-log-access-frontend',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // 3. Criar App Runner Service para o backend
+    const backendService = new apprunner.Service(this, 'BackendService', {
+      source: apprunner.Source.fromEcr({
+        imageConfiguration: { port: 3000 },
+        repository: backendRepository,
+        tagOrDigest: 'latest', // ou uma tag específica
+      }),
+      serviceName: 'cloud-log-backend-service',
+    });
+
+    // 4. Criar outputs
+    new cdk.CfnOutput(this, 'BucketName', {
+      value: logBucket.bucketName,
+      description: 'The name of the S3 bucket for logs.',
+    });
+
+    new cdk.CfnOutput(this, 'BackendRepositoryUri', {
+      value: backendRepository.repositoryUri,
+      description: 'The URI of the backend ECR repository.',
+    });
+
+    new cdk.CfnOutput(this, 'FrontendRepositoryUri', {
+      value: frontendRepository.repositoryUri,
+      description: 'The URI of the frontend ECR repository.',
+    });
+
+    new cdk.CfnOutput(this, 'AppRunnerServiceUrl', {
+      value: backendService.serviceUrl,
+      description: 'The URL of the App Runner service for the backend.',
+    });
+  }
+}
